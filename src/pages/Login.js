19f0840a480f419b6abbb440b7c581c8b2f9c07e
migzdiaz11 <migzdiaz11@gmail.com>
2026-01-18 09:59:41 +0800

    remove the autofill in login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -9,7 +9,7 @@ import PasswordInput from "../ui/PasswordInput";
 
 function Login() {
   const navigate = useNavigate();
-  const [form, setForm] = useState({ email: "jbdtolentino@gmail.com", password: "Panget213!" });
+  const [form, setForm] = useState({ email: "", password: "" });
 
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
