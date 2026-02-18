64652dfa7bf23e08a35e67c8319585f0f73a3a3d
Edrabols <edramiguel11@gmail.com>
2026-02-18 08:27:08 +0800

    v91 simple fix

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -28,7 +28,7 @@ function Login() {
         if (userData.role === "admin" || userData.role === "librarian") {
           navigate("/admin-dashboard", { replace: true });
         } else {
-          navigate("/user-home", { replace: true });
+          navigate("/user-homepage", { replace: true });
         }
       } catch (e) {
         console.error("Error parsing user data:", e);
